fe0fa202071a687aee94283669719160164b5e88
Nir Magnezi <nmagnezi@redhat.com>
2022-08-08 12:56:27 +0300

    Improve mac address randomizer

--- nmstate_configs/generate.py
+++ nmstate_configs/generate.py
@@ -14,6 +14,8 @@ OUTPUT_NAME = 'nmstateconfig'
 OUTPUT_SUFFIX = 'yaml'
 OUTPUT_FOLDER = './output'
 
+NAMESPACE = 'spoke-cluster'
+
 env = Environment(loader=FileSystemLoader(TEMPLATE_FOLDER))
 template = env.get_template(TEMPLATE_NAME)
 
@@ -39,19 +41,27 @@ def delete_files(folder):
             LOG.error('Failed to delete %s. Reason: %s' % (file_path, e))
 
 
-def generate_files(config_amount, folder, output_name, output_suffix):
+def mac_address_generator():
+    return '02:00:%02x:%02x:%02x:%02x' % (
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255),
+    )
+
+
+def generate_files(namespace, config_amount, folder, output_name, output_suffix):
     for i in range(1, config_amount+1):
         c_octet = random.randint(1, 254)
         d_octet = random.randint(1, 254)
-        mac_a = hex(c_octet)[2:]
-        mac_b = hex(d_octet)[2:]
-        mac_a = f'0{mac_a}' if len(mac_a) == 1 else mac_a  # pad with 0 if needed
-        mac_b = f'0{mac_b}' if len(mac_b) == 1 else mac_b  # pad with 0 if needed
+        mac_a = mac_address_generator()
+        mac_b = mac_address_generator()
 
         LOG.debug(f"c_octet: {c_octet} ; d_octet: {d_octet} ; mac_a: {mac_a} ; mac_b: {mac_b}")
 
         output_from_parsed_template = template.render(
             i=i,
+            namespace=namespace,
             c_octet=c_octet,
             d_octet=d_octet,
             mac_a=mac_a,
@@ -64,7 +74,7 @@ def generate_files(config_amount, folder, output_name, output_suffix):
 
 def main():
     delete_files(OUTPUT_FOLDER)
-    generate_files(CONFIGS_AMOUNT, OUTPUT_FOLDER, OUTPUT_NAME, OUTPUT_SUFFIX)
+    generate_files(NAMESPACE, CONFIGS_AMOUNT, OUTPUT_FOLDER, OUTPUT_NAME, OUTPUT_SUFFIX)
     LOG.info(f"Created {CONFIGS_AMOUNT} NMStateConfig files at {OUTPUT_FOLDER}")
 
 
